c388d447baf0ceddc33413879e25f9d2f8e7f02e
Paisit Jarunnamsiri <paisit.j@gmail.com>
2020-02-19 12:10:19 +0700

    add Enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
+Enzyme.configure({ adapter: new Adapter() });
+
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const wrapper = shallow(<App />);
+  expect(wrapper).toBeTruthy();
 });
